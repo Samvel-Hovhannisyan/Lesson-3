caae41ddb3ad6c99622fb1a33b281e61669aacb9
Your Name <you@example.com>
2018-10-14 11:43:06 +0400

    I Add Files

--- index.js
+++ index.js
@@ -20,8 +20,8 @@
 //     console.log("Example is running on port 3000");
 // })
 
-var express = require("express");
-var app = express();
+// var express = require("express");
+// var app = express();
 
 // app.get("/", function (req, res) {
 //     res.send("<h1>Hello world</h1>")
@@ -41,12 +41,65 @@ var app = express();
 //     res.redirect('http://google.com/search?q=' + search);
 // });
 
-app.use(express.static("public"));
+// app.use(express.static("public"));
 
-app.get("/", function(req, res){
-    res.redirect("public");
-});
+// app.get("/", function(req, res){
+//     res.redirect("public");
+// });
 
-app.listen(3000, function () {
-    console.log("Example is running on port 3000");
-});
+// var Square = require("./module");
+// var mySquareObject = new Square(5);
+
+// function main() {
+//     console.log(mySquareObject.getArea());
+// }
+// main();
+
+// app.listen(3000, function () {
+//     console.log("Example is running on port 3000");
+// });
+
+// var fs = require('fs');
+
+// function main() {
+//     var file = "hello.txt";
+//     fs.appendFileSync(file, "Hello world\n");
+// }
+// main();
+
+// var fs = require('fs');
+// var dummyText = "Apple yep";
+
+// function main() {
+//     fs.writeFileSync("dummytext.txt", dummyText);
+//     var text = fs.readFileSync("dummytext.txt").toString();
+//     console.log(dummyText == text);
+//     console.log(text);
+//     fs.writeFileSync("undummytext.txt",
+//         text.replace("Apple", "Microsoft")
+//     );
+// }
+
+// main();
+
+// var fs = require('fs');
+
+// var obj = {
+//     "first_name": "Vardan",
+//     "last_name": "Hovsepyan",
+//     "age": 13,
+//     "tumo_student": true
+// }
+
+// var txt_obj = JSON.stringify(obj);
+
+// function main() {
+//     fs.writeFileSync("obj.json", txt_obj);
+//     var text = fs.readFileSync("obj.json").toString();
+//     console.log(txt_obj == text);
+//     console.log(text);
+// }
+
+// main();
+
+
